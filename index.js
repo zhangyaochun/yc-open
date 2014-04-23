3a0363bc0bd7fcf43e398afc835013ac751cc1f6
zhangyaochun <zhangyaochun@wandoujia.com>
2014-04-23 16:42:58 +0800

    add file first time

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var open = require('open');
+
+
+function callback(error) {
+    if (error != null) {
+        console.error(error);
+    }
+}
+
+function prepareOpts(opts) {
+    opts = opts || {};
+
+    opts.delay = opts.delay || 0;
+
+    //i insist use Canary for test
+    opts.app = opts.app || "Google Chrome Canary";
+
+}
+
+
+module.exports = function(opts) {
+    
+    opts = prepareOpts(opts);
+
+    var url = opts.url || opts.path;
+    var app = opts.app;
+
+    open(url, app, callback);
+};
